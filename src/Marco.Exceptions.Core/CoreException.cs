3136a37dd342034ba53f800bf6177ce1904c94bc
Marco Aurélio <maureliobrasil@gmail.com>
2019-01-25 14:20:19 -0200

    add files

---
+++ src/Marco.Exceptions.Core/CoreException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Marco.Exceptions.Core
+{
+    [Serializable]
+    public abstract class CoreException : Exception
+    {
+        public abstract string Key { get; }
+        public abstract override string Message { get; }
+
+        protected CoreException() : base()
+        {
+        }
+        protected CoreException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/Marco.Exceptions.Core/CoreExceptionGeneric.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Marco.Exceptions.Core
+{
+    [Serializable]
+    public abstract class CoreException<T> : CoreException where T : CoreExceptionItem
+    {
+        public override string Message => $"One or more core exceptions occurred while processing the request. Please check '{nameof(Items)}' for details.";
+
+        private readonly IEnumerable<T> _items;
+        public IReadOnlyCollection<T> Items => new ReadOnlyCollection<T>(_items.ToList());       
+
+        protected CoreException(IEnumerable<T> coreExceptionItems) : base()
+        {
+            _items = coreExceptionItems;
+        }
+
+        protected CoreException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
